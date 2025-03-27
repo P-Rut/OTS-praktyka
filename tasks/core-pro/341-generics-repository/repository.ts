fb82e881495c36f6d366f7e169ee920385ae6e44
Piotr Rutkowski <rutkowski.2387@gmail.com>
2025-03-27 18:47:55 +0100

    fix(341) Solved

--- tasks/core-pro/341-generics-repository/repository.ts
+++ tasks/core-pro/341-generics-repository/repository.ts
@@ -70,4 +70,47 @@ export class ProductRepository {
   }
 }
 
-export class Repository<T extends { id?: number }> {}
+export interface Identifiable {
+  id?: number;
+}
+export interface User extends Identifiable {
+  name: string;
+  email: string;
+}
+export interface Product extends Identifiable {
+  name: string;
+  price: number;
+}
+
+export class Repository<T extends Identifiable> {
+  private dataAccess: DataAccess;
+  private tableName: string;
+
+  constructor(dataAcces: DataAccess, tableName: string) {
+    this.dataAccess = dataAcces;
+    this.tableName = tableName;
+  }
+
+  async getById(id: number): Promise<T> {
+    const query = `SELECT * FROM ${this.tableName} WHERE id = $1`;
+    const values = [id];
+    const res = await this.dataAccess.query<T>(query, values);
+    return res.rows[0] as T;
+  }
+
+  async getAll(): Promise<T[]> {
+    const query = `SELECT * FROM ${this.tableName}`;
+    const res = await this.dataAccess.query<T>(query);
+    return res.rows;
+  }
+
+  async insert(item: T): Promise<T> {
+    const columns = Object.keys(item).filter((key) => key != 'id');
+    const values = columns.map((_, i) => `$${i + 1}`);
+    const placeholders = columns.map((col) => (item as any)[col]);
+    const query = `INSERT INTO ${this.tableName} (${columns.join(',')}) VALUES (${values.join(',')}) RETURNING *`;
+
+    const res = await this.dataAccess.query<T>(query, placeholders);
+    return res.rows[0];
+  }
+}
