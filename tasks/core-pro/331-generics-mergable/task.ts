6f7cd0a656460610a11e4e5a2bf1df71c0290838
Piotr Rutkowski <rutkowski.2387@gmail.com>
2025-03-27 18:09:57 +0100

    fix(331) Solved

--- tasks/core-pro/331-generics-mergable/task.ts
+++ tasks/core-pro/331-generics-mergable/task.ts
@@ -1,7 +1,10 @@
 // Zaimplementuj typ MergeableObject z wykorzystaniem typów wbudowanych - Exclude i NonNullable.
-type MergeableObject<T> = T;
+type MergeableObject<T> = Exclude<NonNullable<T>, Function | string | number | boolean | symbol>;
 
-export function mergeObjects<T, U>(obj1: MergeableObject<T>, obj2: MergeableObject<U>): T & U {
+export function mergeObjects<T extends object, U extends object>(
+  obj1: MergeableObject<T>,
+  obj2: MergeableObject<U>,
+): T & U {
   const merged = { ...obj1, ...obj2 };
   console.log(merged);
   return merged;
