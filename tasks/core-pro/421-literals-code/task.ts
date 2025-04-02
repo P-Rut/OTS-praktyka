8bbb8959a9e24cf43b08b4ac1bde374fac300b7e
Piotr Rutkowski <rutkowski.2387@gmail.com>
2025-04-02 19:08:14 +0200

    fix(421) Solved

--- tasks/core-pro/421-literals-code/task.ts
+++ tasks/core-pro/421-literals-code/task.ts
@@ -1,5 +1,27 @@
-export type Code = null;
+import { split } from 'postcss/lib/list';
+
+export type Code = `${number}-${number}-${number}`;
 
 export function codeToDecimal(code: Code) {
-  throw new Error('Not implemented');
+  const formattedCode = code.split('-');
+
+  function count(code: string[]) {
+    let summary: number = 0;
+    const weight = [4, 2, 1];
+    code.forEach((value, index) => {
+      summary += Number(value) * weight[index];
+    });
+    return summary;
+  }
+  // function count(code: string[]) {
+  //   let summary = 0;
+  //   const weights = [4, 2, 1];
+
+  //   for (let i = 0; i < code.length; i++) {
+  //     summary += Number(code[i]) * weights[i];
+  //   }
+
+  //   return summary;
+  // }
+  return `${count([...formattedCode[0]])}${count([...formattedCode[1]])}${count([...formattedCode[2]])}`;
 }
